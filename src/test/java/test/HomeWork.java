dad7cc1bdd632f6a322e0d04dc528cd9531f8264
“[Nurjana <[nurjana.a@gmail.com]>
2020-06-25 17:05:47 -0500

    hw

---
+++ src/test/java/test/HomeWork.java
@@ -0,0 +1,19 @@
+package test;
+
+public class HomeWork {
+}
+/*
+TC #1: Smartbear software link verification
+1. Open browser
+2. Go to website:
+http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx
+3. Enter username: “Tester”
+4. Enter password: “test”
+5. Click to Login button
+6. Print out count of all the links on landing page
+7. Print out each link text on this page
+Mini-Task:
+• Create a method called loginToSmartBear
+• This method simply logs in to SmartBear when you call it.
+• Accepts WebDriver type as parameter
+ */
